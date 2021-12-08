8b1b7f102e63c544ba4403768cb90fde1d2d46b2
Emiliano Menéndez <emystein@gmail.com>
2021-12-08 11:21:07 -0300

    Select stocks using AsyncPaginate.

--- src/renderer/Search.tsx
+++ src/renderer/Search.tsx
@@ -1,8 +1,8 @@
-import {useState, useEffect} from 'react';
+import {useRef, useState, useEffect} from 'react';
 import axios from 'axios';
-import Select from 'react-select';
+import {AsyncPaginate} from 'react-select-async-paginate';
 
-const searchStockSymbol = (keywords: string) => {
+const searchStockSymbol = async (keywords: string) => {
   // TODO parameterize
   const apiKey = '2SG3G2I68SLNY6ZL';
   const apiUrl = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${apiKey}`;
@@ -10,56 +10,37 @@ const searchStockSymbol = (keywords: string) => {
 };
 
 const Search = () => {
-  const [searchKeywords, setSearchKeywords] = useState('');
-  const [stockSelectOptions, setStockSelectOptions] = useState([]);
-  const [selectedStockSymbol] = useState('');
-
-  const handleChangeKeywords = (event) => {
-    setSearchKeywords(event.target.value);
-  };
+  const [selectedStock, setSelectedStock] = useState(null);
 
   const stockSelectOption = () => (match) => ({
     value: match['1. symbol'],
     label: match['2. name'],
   });
 
-  const stockSelectOptionsFromApiResponse = (response) =>
+  const stockSelectOptionsFrom = (response) =>
     response.data.bestMatches.map(stockSelectOption());
 
-  const searchOnEvent = (event) => {
-    event.preventDefault();
-    document.body.style.cursor = 'wait';
-    searchStockSymbol(searchKeywords)
-      .then((response) => {
-        setStockSelectOptions(stockSelectOptionsFromApiResponse(response));
-      })
-      .catch(() => {
-        setStockSelectOptions([]);
-      })
-      .finally(() => {
-        document.body.style.cursor = 'default';
-      });
+  const loadOptions = async (searchKeywords, loadedOptions) => {
+    if (!searchKeywords.trim()) {
+      return { options: [], hasMore: false };
+    }
+
+    const response = await searchStockSymbol(searchKeywords);
+
+    return {
+      options: stockSelectOptionsFrom(response),
+      hasMore: false,
+    };
   };
 
   return (
     <div className="Main">
-      <form onSubmit={searchOnEvent}>
-        <div className="mb-3">
-          <input
-            type="text"
-            placeholder="keywords"
-            value={searchKeywords}
-            onChange={handleChangeKeywords}
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <button type="submit" className="btn btn-primary">
-            Search
-          </button>
-        </div>
-      </form>
-      <Select options={stockSelectOptions} value={selectedStockSymbol}/>
+      <AsyncPaginate
+        value={selectedStock}
+        loadOptions={loadOptions}
+        onChange={setSelectedStock}
+        debounceTimeout={1000}
+      />
     </div>
   );
 };
