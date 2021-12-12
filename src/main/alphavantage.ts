565075e62a5ba62ed5068d9944a7b440d8575fb0
Emiliano Menéndez <emystein@gmail.com>
2021-12-12 19:32:50 -0300

    Extract alphavantage.ts.

---
+++ src/main/alphavantage.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const apiKey = process.env.ALPHAVANTAGE_API_KEY;
+
+const searchUrl = (keywords: string) => {
+  return `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${apiKey}`;
+};
+
+export const searchStockSymbol = async (keywords: string) => {
+  if (!keywords.trim()) {
+    return [];
+  }
+
+  return axios.get(searchUrl(keywords));
+};
+
+export const stockSelectOptionsFrom = (response) =>
+  response.data.bestMatches.map(matchToSelectOption());
+
+const matchToSelectOption = () => (match) => ({
+  value: match['1. symbol'],
+  label: match['2. name'],
+});
--- src/renderer/Search.tsx
+++ src/renderer/Search.tsx
@@ -1,29 +1,11 @@
 import { useState } from 'react';
-import axios from 'axios';
 import { AsyncPaginate } from 'react-select-async-paginate';
-
-const searchStockSymbol = async (keywords: string) => {
-  const apiKey = process.env.ALPHAVANTAGE_API_KEY;
-  const apiUrl = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${apiKey}`;
-  return axios.get(apiUrl);
-};
+import { searchStockSymbol, stockSelectOptionsFrom } from '../main/alphavantage';
 
 const Search = () => {
   const [selectedStock, setSelectedStock] = useState(null);
 
-  const stockSelectOption = () => (match) => ({
-    value: match['1. symbol'],
-    label: match['2. name'],
-  });
-
-  const stockSelectOptionsFrom = (response) =>
-    response.data.bestMatches.map(stockSelectOption());
-
   const loadOptions = async (searchKeywords, loadedOptions) => {
-    if (!searchKeywords.trim()) {
-      return {options: []};
-    }
-
     const response = await searchStockSymbol(searchKeywords);
 
     return {options: stockSelectOptionsFrom(response)};
