0690c2a5144788de777678e06ea4068b656b55c0
Emiliano Menéndez <emystein@gmail.com>
2022-08-07 14:37:41 -0300

    Fill list with stock search results.

--- src/renderer/Search.tsx
+++ src/renderer/Search.tsx
@@ -3,11 +3,16 @@ import { AsyncPaginate } from 'react-select-async-paginate';
 
 const Search = ({ search, selectOptionMap }: Props) => {
   const [selected, setSelected] = useState(null);
+  const [list, setList] = useState([]);
 
   const loadOptions = async (searchKeywords, loadedOptions) => {
     const response = await search(searchKeywords);
 
     return {options: selectOptionMap(response)};
+  };
+
+  const addSelectedToList = () => {
+    setList(list.concat(selected));
   };
 
   return (
@@ -17,10 +22,18 @@ const Search = ({ search, selectOptionMap }: Props) => {
         value={selected}
         loadOptions={loadOptions}
         onChange={setSelected}
-        debounceTimeout={1000}
+        debounceTimeout={2000}
         classNamePrefix="search"
         className="search-container"
       />
+
+      <button onClick={addSelectedToList}>Add</button>
+
+      <ul className="list-group">
+        {list.map((item) => (
+          <li key={item.value}>{item.label}</li>
+        ))}
+      </ul>
     </div>
   );
 };
